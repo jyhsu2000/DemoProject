280a42cf285c31de7f6c78c1d5999b5284c1bf24
jyhsu <jyhsu2000@gmail.com>
2023-10-18 16:37:30 +0800

    Add application plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     id("java")
+    application
 }
 
 group = "com.github.jyhsu2000"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "com.github.jyhsu2000.Main"
+}
 
 repositories {
     mavenCentral()
@@ -14,6 +19,17 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
+tasks {
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    compileTestJava {
+        options.encoding = "UTF-8"
+    }
+}
 tasks.test {
     useJUnitPlatform()
 }
